069bca045f871749a9a9b90d94de87083b2b7462
Mateo Baca <mateobacas07@gmail.com>
2025-02-03 16:22:09 -0500

    Schemas added

---
+++ src/schemas/contentSchema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const contentSchema = new mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    parameterID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Parameter',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const content = mongoose.model('Content', contentSchema);
+export default content;
---
+++ src/schemas/parameterSchema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const parameterSchema = mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    keywords: [String],
+    length: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const parameter = mongoose.model('Parameter', parameterSchema);
+export default parameter;
