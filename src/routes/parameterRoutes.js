fb59eab87ed0c44a910b5a6330b456f0c32f3526
Mateo Baca <mateobacas07@gmail.com>
2025-02-04 18:24:48 -0500

    FindByID and FindByCategory routes added to parameterController

--- src/routes/parameterRoutes.js
+++ src/routes/parameterRoutes.js
@@ -6,6 +6,10 @@ const parameterRoutes = express.Router();
 
 parameterRoutes.get('/all', verifyToken, (req, res) => parameterController.find(req, res));
 
+parameterRoutes.get('/:id', verifyToken, (req, res) => parameterController.findByID(req, res));
+
+parameterRoutes.get('/category', verifyToken, (req, res) => parameterController.findByCategory(req, res));
+
 parameterRoutes.post('/create', verifyToken, (req, res) => parameterController.create(req, res));
 
 export default parameterRoutes;
