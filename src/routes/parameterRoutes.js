486084a80d6a9f8bc67e783880ab34ab479c0e04
Mateo Baca <mateobacas07@gmail.com>
2025-02-04 18:40:04 -0500

    Error fix with FindByCategory in parameterController and parameterRoutes

--- src/routes/parameterRoutes.js
+++ src/routes/parameterRoutes.js
@@ -8,7 +8,7 @@ parameterRoutes.get('/all', verifyToken, (req, res) => parameterController.find(
 
 parameterRoutes.get('/:id', verifyToken, (req, res) => parameterController.findByID(req, res));
 
-parameterRoutes.get('/category', verifyToken, (req, res) => parameterController.findByCategory(req, res));
+parameterRoutes.get('/category/:category', verifyToken, (req, res) => parameterController.findByCategory(req, res));
 
 parameterRoutes.post('/create', verifyToken, (req, res) => parameterController.create(req, res));
 
