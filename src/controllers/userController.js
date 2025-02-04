43673c3de1f68c6fcb02b762f741b3960015d260
Mateo Baca <mateobacas07@gmail.com>
2025-02-03 21:22:12 -0500

    UpdatedByID user method added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,10 +39,10 @@ class userController {
                 return res.status(404).json({ error: 'User cannot update or user doesnt exist' });
             }
 
-            return res.status ()
+            return res.status(200).json({ 'message': 'User created' });
 
         } catch (e) {
-            
+            return res.status(500).json({ error: 'Error updating user' });
         }
     }
 
