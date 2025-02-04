a929d9ee96aace34f5a78f16074565269652845e
Mateo Baca <mateobacas07@gmail.com>
2025-02-03 21:28:02 -0500

    DeleteByID method added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -36,10 +36,10 @@ class userController {
             const new_user = await userModel.updateById(id, res.body);
 
             if (!new_user) {
-                return res.status(404).json({ error: 'User cannot update or user doesnt exist' });
+                return res.status(404).json({ error: 'User cannot be update or user doesnt exist' });
             }
 
-            return res.status(200).json({ 'message': 'User created' });
+            return res.status(201).json({ 'message': 'User created' });
 
         } catch (e) {
             return res.status(500).json({ error: 'Error updating user' });
@@ -47,11 +47,23 @@ class userController {
     }
 
     async deleteByID (req, res) {
+        try {
+            const { id } = req.params;
+            const deleted_user = await userModel.deleteById(id);
+            
+            if (!deleted_user) {
+                return res.status(404).json({ error: 'User cannot be delete or user doesnt exist' });
+            }
 
+            return res.status(204).json({ message: 'User deleted' });
+
+        } catch (e) {
+            
+        }
     }
 
     async register (req, res) {
-
+        
     }
 
     async login (req, res) {
