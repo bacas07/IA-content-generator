1b486d0bc089fca4be5c318d76513d4b53504798
Mateo Baca <mateobacas07@gmail.com>
2025-02-04 17:22:21 -0500

    Verify token implemented on users routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
 import userController from "../controllers/userController.js";
+import { verifyToken } from "../utils/auth.js";
 
 const userRoutes = express.Router();
 
-userRoutes.get('/', (req, res) => userController.find(req, res));
+userRoutes.get('/all', verifyToken, (req, res) => userController.find(req, res));
 
-userRoutes.get('/:id', (req, res) => userController.findByID(req, res));
+userRoutes.get('/:id', verifyToken, (req, res) => userController.findByID(req, res));
 
-userRoutes.put('/:id', (req, res) => userController.updateByID(req, res));
+userRoutes.put('/:id', verifyToken, (req, res) => userController.updateByID(req, res));
 
-userRoutes.delete('/:id', (req, res) => userController.deleteByID(req, res));
+userRoutes.delete('/:id', verifyToken, (req, res) => userController.deleteByID(req, res));
 
 userRoutes.post('/register', (req, res) => userController.register(req, res));
 
