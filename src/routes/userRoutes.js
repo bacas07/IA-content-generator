115e815cfcc10fe51ba27b8f46aa20a72074d5d8
Mateo Baca <mateobacas07@gmail.com>
2025-02-03 22:41:22 -0500

    All users routes added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,6 +7,10 @@ userRoutes.get('/', (req, res) => userController.find(req, res));
 
 userRoutes.get('/:id', (req, res) => userController.findByID(req, res));
 
+userRoutes.put('/:id', (req, res) => userController.updateByID(req, res));
+
+userRoutes.delete('/:id', (req, res) => userController.deleteByID(req, res));
+
 userRoutes.post('/register', (req, res) => userController.register(req, res));
 
 userRoutes.post('/log', (req, res) => userController.login(req, res));
