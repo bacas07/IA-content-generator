38858709a9d325debbbfc1a558fbfb2d4ebc597b
Mateo Baca <mateobacas07@gmail.com>
2025-02-04 17:34:53 -0500

    findByID method added to parameterController

--- src/controllers/parameterController.js
+++ src/controllers/parameterController.js
@@ -11,8 +11,19 @@ class parameterController {
         }
     }
 
-    async findByID () {
+    async findByID (req, res) {
+        try {
+            const { id } = req.params;
+            const parameter = await parameterModel.findById(id);
 
+            if (!parameter) {
+                return res.status(404).json({ error: 'Parameter doesnt exist' });
+            }
+
+            return res.status(200).json(parameter);
+        } catch (e) {
+            return res.status(500).json({ error: 'Error finding parameter' });
+        }
     }
 
     async create () {
