1350c7b2e9302e8a02758d2a05bb974108e99549
Mateo Baca <mateobacas07@gmail.com>
2025-02-07 13:18:52 -0500

    FindByID method added to contentController

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -11,6 +11,22 @@ class contentController {
             return res.status(500).json({ error: 'Error finding all content' });
         }
     }
+
+    async findByID (req, res) {
+        try {
+            const { id } = req.params;
+            const content = await contentModel.findById(id);
+
+            if (!content) {
+                return res.status(404).json({ error: 'Content doesnt exist' });
+            }
+
+            return res.status(200).json(content)
+        } catch (e) {
+            console.error('Error: ', e);
+            return res.status(500).json({ error: 'Error finding content' });            
+        }
+    }
 }
 
 export default new contentController();
