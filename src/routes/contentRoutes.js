36f90626be3d028eb633e3cc78055a7091abc5ac
Mateo Baca <mateobacas07@gmail.com>
2025-02-08 12:33:18 -0500

    Create content route added to contentRouter

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -6,4 +6,6 @@ const contentRouter = express.Router();
 
 contentRouter.get('/all', verifyToken, (req, res) => contentController.find(req, res));
 
+contentRouter.post('/create/:parameter_id', verifyToken, (req, res) => contentController.create(req, res));
+
 export default contentRouter;
