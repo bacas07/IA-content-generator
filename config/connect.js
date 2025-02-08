f6a04d23f2004901a224e741a42d65a2bf718d1f
Mateo Baca <mateobacas07@gmail.com>
2025-02-08 12:10:39 -0500

    Console logs server moddifieds

--- config/connect.js
+++ config/connect.js
@@ -6,7 +6,7 @@ dotenv.config()
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
-        console.log('--> Server connected to database');
+        console.log('> Server connected to database');
     } catch (e) {
         console.error('Error: ', e);
     }
--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use('/content', contentRouter);
 try {
     const PORT = process.env.PORT || 5000
     app.listen(PORT, () => {
-        console.log('--> Server running on port 3000');
+        console.log('> Server running on port 3000');
         connectDB();
     })
 } catch (e) {
