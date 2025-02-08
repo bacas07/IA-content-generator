de643489305f1502ae59073bbcca96ff1868fa5c
Mateo Baca <mateobacas07@gmail.com>
2025-02-08 13:06:06 -0500

    User endpoints modified

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,9 +8,9 @@ userRoutes.get('/all', verifyToken, (req, res) => userController.find(req, res))
 
 userRoutes.get('/:id', verifyToken, (req, res) => userController.findByID(req, res));
 
-userRoutes.put('update/:id', verifyToken, (req, res) => userController.updateByID(req, res));
+userRoutes.put('/update/:id', verifyToken, (req, res) => userController.updateByID(req, res));
 
-userRoutes.delete('delete/:id', verifyToken, (req, res) => userController.deleteByID(req, res));
+userRoutes.delete('/delete/:id', verifyToken, (req, res) => userController.deleteByID(req, res));
 
 userRoutes.post('/register', (req, res) => userController.register(req, res));
 
