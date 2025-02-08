2bc9ebd24946f5b1ed39648a4ff99e11c9a3922c
Mateo Baca <mateobacas07@gmail.com>
2025-02-08 13:02:53 -0500

    User endpoints modified

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,11 +6,11 @@ const userRoutes = express.Router();
 
 userRoutes.get('/all', verifyToken, (req, res) => userController.find(req, res));
 
-userRoutes.get('/update/:id', verifyToken, (req, res) => userController.findByID(req, res));
+userRoutes.get('/:id', verifyToken, (req, res) => userController.findByID(req, res));
 
-userRoutes.put('/delete/:id', verifyToken, (req, res) => userController.updateByID(req, res));
+userRoutes.put('update/:id', verifyToken, (req, res) => userController.updateByID(req, res));
 
-userRoutes.delete('/:id', verifyToken, (req, res) => userController.deleteByID(req, res));
+userRoutes.delete('delete/:id', verifyToken, (req, res) => userController.deleteByID(req, res));
 
 userRoutes.post('/register', (req, res) => userController.register(req, res));
 
