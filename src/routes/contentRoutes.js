04c57fa066d87e0c26ab6b8aa459d47723d5054a
Mateo Baca <mateobacas07@gmail.com>
2025-02-10 17:44:24 -0500

    Content routes fixed

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -2,20 +2,20 @@ import express from "express";
 import contentController from "../controllers/contentController.js";
 import { verifyToken } from "../utils/auth.js";
 
-const contentRouter = express.Router();
+const contentRoutes = express.Router();
 
-contentRouter.get('/all', verifyToken, (req, res) => contentController.find(req, res));
+contentRoutes.get('/all', verifyToken, (req, res) => contentController.find(req, res));
 
-contentRouter.get('/:id', verifyToken, (req, res) => contentController.findByID(req, res));
+contentRoutes.get('/:id', verifyToken, (req, res) => contentController.findByID(req, res));
 
-userRoutes.get('/all/unactive', verifyToken, (req, res) => contentController.findUnactive(req, res));
+contentRoutes.get('/all/unactive', verifyToken, (req, res) => contentController.findUnactive(req, res));
 
-contentRouter.get('/user/:id', verifyToken, (req, res) => contentController.findByUserID(req, res));
+contentRoutes.get('/user/:id', verifyToken, (req, res) => contentController.findByUserID(req, res));
 
-contentRouter.get('/parameter/:id', verifyToken, (req, res) => contentController.findByParameterID(req, res));
+contentRoutes.get('/parameter/:id', verifyToken, (req, res) => contentController.findByParameterID(req, res));
 
-contentRouter.get('/category/:category', verifyToken, (req, res) => contentController.findByCategory(req, res));
+contentRoutes.get('/category/:category', verifyToken, (req, res) => contentController.findByCategory(req, res));
 
-contentRouter.post('/create/:parameter_id', verifyToken, (req, res) => contentController.create(req, res));
+contentRoutes.post('/create/:parameter_id', verifyToken, (req, res) => contentController.create(req, res));
 
-export default contentRouter;
+export default contentRoutes;
