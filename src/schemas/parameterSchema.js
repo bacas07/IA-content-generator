5d647466affe7d53c0122f4a246d582ce05f4627
Mateo Baca <mateobacas07@gmail.com>
2025-02-10 16:55:30 -0500

    Is_active added to parameterSchema

--- src/schemas/parameterSchema.js
+++ src/schemas/parameterSchema.js
@@ -14,6 +14,10 @@ const parameterSchema = mongoose.Schema({
     length: {
         type: Number,
         required: true
+    },
+    is_active: {
+        type: Boolean,
+        default: true
     }
 }, {
     timestamps: true
