9b0ce13a2cdc5aa87b7cdef60ab5dc87cfa190d0
Mateo Baca <mateobacas07@gmail.com>
2025-02-10 17:45:22 -0500

    Parameter routes fixed

--- src/routes/parameterRoutes.js
+++ src/routes/parameterRoutes.js
@@ -8,7 +8,7 @@ parameterRoutes.get('/all', verifyToken, (req, res) => parameterController.find(
 
 parameterRoutes.get('/:id', verifyToken, (req, res) => parameterController.findByID(req, res));
 
-userRoutes.get('/all/unactive', verifyToken, (req, res) => parameterController.findUnactive(req, res));
+parameterRoutes.get('/all/unactive', verifyToken, (req, res) => parameterController.findUnactive(req, res));
 
 parameterRoutes.get('/user/:id', verifyToken, (req, res) => parameterController.findByUserID(req, res));
 
