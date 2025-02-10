92cbc756ca929ce038afe3f9f1ff9cf6cc064ec0
Mateo Baca <mateobacas07@gmail.com>
2025-02-10 17:30:16 -0500

    Method finUnactive added to contentModel

--- src/models/contentModel.js
+++ src/models/contentModel.js
@@ -9,6 +9,14 @@ class contentModel {
         }
     }
 
+    async findUnactive () {
+        try {
+            return await user.find({ is_active: false });
+        } catch (e) {
+            console.error('error: ', e);
+        }
+    }
+
     async findById (id) {
         try {
             return await content.findById(id);
