3d92c8ca0ea788f4fc5ca0f7d57e5110b5d3a5e2
Mateo Baca <mateobacas07@gmail.com>
2025-02-21 00:04:32 -0500

    Roles added to contentRoutes

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -1,23 +1,23 @@
 import express from "express";
 import contentController from "../controllers/contentController.js";
-import { verifyToken } from "../utils/auth.js";
+import { permit, verifyToken } from "../utils/auth.js";
 
 const contentRoutes = express.Router();
 
-contentRoutes.get('/all', verifyToken, (req, res) => contentController.find(req, res));
+contentRoutes.get('/all', verifyToken, permit('admin', 'developer', 'user'), (req, res) => contentController.find(req, res));
 
-contentRoutes.get('/:id', verifyToken, (req, res) => contentController.findByID(req, res));
+contentRoutes.get('/:id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => contentController.findByID(req, res));
 
-contentRoutes.get('/all/unactive', verifyToken, (req, res) => contentController.findUnactive(req, res));
+contentRoutes.get('/all/unactive', verifyToken, permit('admin'), (req, res) => contentController.findUnactive(req, res));
 
-contentRoutes.get('/user/:id', verifyToken, (req, res) => contentController.findByUserID(req, res));
+contentRoutes.get('/user/:id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => contentController.findByUserID(req, res));
 
-contentRoutes.get('/parameter/:id', verifyToken, (req, res) => contentController.findByParameterID(req, res));
+contentRoutes.get('/parameter/:id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => contentController.findByParameterID(req, res));
 
-contentRoutes.get('/category/:category', verifyToken, (req, res) => contentController.findByCategory(req, res));
+contentRoutes.get('/category/:category', verifyToken, permit('admin', 'developer', 'user'), (req, res) => contentController.findByCategory(req, res));
 
-contentRoutes.post('/create/:parameter_id', verifyToken, (req, res) => contentController.create(req, res));
+contentRoutes.post('/create/:parameter_id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => contentController.create(req, res));
 
-contentRoutes.delete('/delete/:id', verifyToken, (req, res) => contentController.deleteByID(req, res));
+contentRoutes.delete('/delete/:id', verifyToken, permit('admin'), (req, res) => contentController.deleteByID(req, res));
 
 export default contentRoutes;
