b2a56fd89f45642a7ea264b53c09561f7ed77da9
Mateo Baca <mateobacas07@gmail.com>
2025-02-21 10:13:47 -0500

    VerifyApiKey added to userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,21 @@
 import express from "express";
 import userController from "../controllers/userController.js";
-import { verifyToken, permit } from "../utils/auth.js";
+import { verifyToken, permit, verifyApiKey } from "../utils/auth.js";
 
 const userRoutes = express.Router();
 
-userRoutes.get('/all', verifyToken, permit('admin', 'developer'), (req, res) => userController.find(req, res));
+userRoutes.get('/all', verifyApiKey, verifyToken, permit('admin', 'developer'), (req, res) => userController.find(req, res));
 
-userRoutes.get('/:id', verifyToken, permit('admin', 'developer'), (req, res) => userController.findByID(req, res));
+userRoutes.get('/:id', verifyApiKey, verifyToken, permit('admin', 'developer'), (req, res) => userController.findByID(req, res));
 
-userRoutes.get('/all/unactive', verifyToken, permit('admin', 'developer'), (req, res) => userController.findUnactive(req, res));
+userRoutes.get('/all/unactive', verifyApiKey, verifyToken, permit('admin', 'developer'), (req, res) => userController.findUnactive(req, res));
 
-userRoutes.put('/update/:id', verifyToken, permit('admin'), (req, res) => userController.updateByID(req, res));
+userRoutes.put('/update/:id', verifyApiKey, verifyToken, permit('admin'), (req, res) => userController.updateByID(req, res));
 
-userRoutes.delete('/delete/:id', verifyToken, permit('admin'),(req, res) => userController.deleteByID(req, res));
+userRoutes.delete('/delete/:id', verifyApiKey, verifyToken, permit('admin'),(req, res) => userController.deleteByID(req, res));
 
-userRoutes.post('/register', (req, res) => userController.register(req, res));
+userRoutes.post('/register', verifyApiKey, (req, res) => userController.register(req, res));
 
-userRoutes.post('/log', (req, res) => userController.login(req, res));
+userRoutes.post('/log', verifyApiKey, (req, res) => userController.login(req, res));
 
 export default userRoutes;
