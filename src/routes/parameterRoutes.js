1a6d60ed616655dd6b7096829fafe97b1c93fd08
Mateo Baca <mateobacas07@gmail.com>
2025-02-20 23:44:43 -0500

    Roles added to parameterRoutes

--- src/routes/parameterRoutes.js
+++ src/routes/parameterRoutes.js
@@ -1,23 +1,23 @@
 import express from "express";
 import parameterController from "../controllers/parameterController.js";
-import { verifyToken } from "../utils/auth.js";
+import { verifyToken, permit } from "../utils/auth.js";
 
 const parameterRoutes = express.Router();
 
-parameterRoutes.get('/all', verifyToken, (req, res) => parameterController.find(req, res));
+parameterRoutes.get('/all', verifyToken, permit('admin', 'developer'), (req, res) => parameterController.find(req, res));
 
-parameterRoutes.get('/:id', verifyToken, (req, res) => parameterController.findByID(req, res));
+parameterRoutes.get('/:id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.findByID(req, res));
 
-parameterRoutes.get('/all/unactive', verifyToken, (req, res) => parameterController.findUnactive(req, res));
+parameterRoutes.get('/all/unactive', verifyToken, permit('admin'), (req, res) => parameterController.findUnactive(req, res));
 
-parameterRoutes.get('/user/:id', verifyToken, (req, res) => parameterController.findByUserID(req, res));
+parameterRoutes.get('/user/:id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.findByUserID(req, res));
 
-parameterRoutes.get('/category/:category', verifyToken, (req, res) => parameterController.findByCategory(req, res));
+parameterRoutes.get('/category/:category', verifyToken, permit('admin', 'developer'), (req, res) => parameterController.findByCategory(req, res));
 
-parameterRoutes.post('/create', verifyToken, (req, res) => parameterController.create(req, res));
+parameterRoutes.post('/create', verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.create(req, res));
 
-parameterRoutes.put('/update/:id', verifyToken, (req, res) => parameterController.updateByID(req, res));
+parameterRoutes.put('/update/:id', verifyToken, permit('admin'), (req, res) => parameterController.updateByID(req, res));
 
-parameterRoutes.delete('/delete/:id', verifyToken, (req, res) => parameterController.deleteByID(req, res));
+parameterRoutes.delete('/delete/:id', verifyToken, permit('admin'), (req, res) => parameterController.deleteByID(req, res));
 
 export default parameterRoutes;
