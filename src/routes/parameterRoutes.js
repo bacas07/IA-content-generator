a43b2540b5215f5532ff180ba3ea6afcb68f47b5
Mateo Baca <mateobacas07@gmail.com>
2025-02-21 10:14:41 -0500

    VerifyApiKey added to parameterRoutes

--- src/routes/parameterRoutes.js
+++ src/routes/parameterRoutes.js
@@ -1,23 +1,23 @@
 import express from "express";
 import parameterController from "../controllers/parameterController.js";
-import { verifyToken, permit } from "../utils/auth.js";
+import { verifyToken, permit, verifyApiKey } from "../utils/auth.js";
 
 const parameterRoutes = express.Router();
 
-parameterRoutes.get('/all', verifyToken, permit('admin', 'developer'), (req, res) => parameterController.find(req, res));
+parameterRoutes.get('/all', verifyApiKey, verifyToken, permit('admin', 'developer'), (req, res) => parameterController.find(req, res));
 
-parameterRoutes.get('/:id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.findByID(req, res));
+parameterRoutes.get('/:id', verifyApiKey, verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.findByID(req, res));
 
-parameterRoutes.get('/all/unactive', verifyToken, permit('admin'), (req, res) => parameterController.findUnactive(req, res));
+parameterRoutes.get('/all/unactive', verifyApiKey, verifyToken, permit('admin'), (req, res) => parameterController.findUnactive(req, res));
 
-parameterRoutes.get('/user/:id', verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.findByUserID(req, res));
+parameterRoutes.get('/user/:id', verifyApiKey, verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.findByUserID(req, res));
 
-parameterRoutes.get('/category/:category', verifyToken, permit('admin', 'developer'), (req, res) => parameterController.findByCategory(req, res));
+parameterRoutes.get('/category/:category', verifyApiKey, verifyToken, permit('admin', 'developer'), (req, res) => parameterController.findByCategory(req, res));
 
-parameterRoutes.post('/create', verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.create(req, res));
+parameterRoutes.post('/create', verifyApiKey, verifyToken, permit('admin', 'developer', 'user'), (req, res) => parameterController.create(req, res));
 
-parameterRoutes.put('/update/:id', verifyToken, permit('admin'), (req, res) => parameterController.updateByID(req, res));
+parameterRoutes.put('/update/:id', verifyApiKey, verifyToken, permit('admin'), (req, res) => parameterController.updateByID(req, res));
 
-parameterRoutes.delete('/delete/:id', verifyToken, permit('admin'), (req, res) => parameterController.deleteByID(req, res));
+parameterRoutes.delete('/delete/:id', verifyApiKey, verifyToken, permit('admin'), (req, res) => parameterController.deleteByID(req, res));
 
 export default parameterRoutes;
